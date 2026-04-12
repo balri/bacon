b56a9f78b2dde3a798114415d21f014e94e18184
Blair Venn <blair.venn@gmail.com>
2026-04-12 19:17:26 +1000

    Fix build errors

--- frontend/src/utils/cookie.ts
+++ frontend/src/utils/cookie.ts
@@ -11,7 +11,7 @@ type CookieData = {
 	numSolved?: number | null;
 	longestStreak?: number | null;
 	streak?: number | null;
-	stack: Array<{ type: "actor" | "movie"; data: Actor | Movie }>;
+	stack?: Array<{ type: "actor" | "movie"; data: Actor | Movie }>;
 };
 
 export function getTodayDateString() {
